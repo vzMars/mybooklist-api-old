32e52659564a1dfdb8c0a52e5f8b2c071b10bc71
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-15 17:00:27 -0400

    signup method that uses helper method from user model and logs in the user after signing up using passport logIn method

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
+const User = require('../models/User');
+const passport = require('passport');
+
 module.exports = {
   authStatus: (req, res) => {
     res.status(200).json('auth status');
@@ -8,7 +11,20 @@ module.exports = {
   logout: (req, res) => {
     res.status(200).json('logout');
   },
-  signup: (req, res) => {
-    res.status(200).json('signup');
+  signup: async (req, res, next) => {
+    const { email, userName, password } = req.body;
+
+    try {
+      const user = await User.signup(email, userName, password);
+
+      req.logIn(user, (err) => {
+        if (err) return next(err);
+        res.status(200).json({
+          user: { id: user.id, email: user.email, userName: user.userName },
+        });
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   },
 };
