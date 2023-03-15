dafc80ad9588f7f7be1127b4f09a9eb74b8d8b77
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-15 12:19:46 -0400

    enabled cors, added passport middleware and able to connect to database

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/database');
 
+// Load config
 require('dotenv').config({ path: './config/.env' });
+
+// Passport config
+require('./config/passport')(passport);
+
+// Enable CORS
+app.use(cors(corsOptions));
+
+// Database Connection
+connectDB();
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
