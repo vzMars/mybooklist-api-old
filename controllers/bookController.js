370ff5b4c285931214d8c529e328a1b6b8e92fd9
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-22 18:25:44 -0400

    switched to using google books api instead of open library

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,14 +1,17 @@
-const BASEURL = 'https://openlibrary.org/search.json?q=';
+const BASEURL = 'https://books.googleapis.com/books/v1/volumes?q=';
 
 module.exports = {
   searchBooks: async (req, res) => {
     try {
-      const query = req.params.query.replaceAll(' ', '+');
+      const query = req.params.query.replaceAll(' ', '%20');
 
-      const response = await fetch(`${BASEURL}${query}`);
+      const response = await fetch(
+        `${BASEURL}${query}&maxResults=40&key=${process.env.GOOGLE_API_KEY}`
+      );
       const json = await response.json();
+      const books = json.items ? json.items : [];
 
-      res.status(200).json(json.docs);
+      res.status(200).json(books);
     } catch (error) {
       res
         .status(400)
