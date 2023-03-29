dfdc0b62def6644cdeb6ec02fc45c9314a182b48
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-28 22:36:19 -0400

    book schema created

---
+++ models/Book.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  bookId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['currently_reading', 'completed', 'dropped', 'plan_to_read'],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Book', BookSchema);
