535b2de7dd34de9d137ac4b48ff661369466a9e2
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-29 19:06:02 -0400

    added timestamps to book schema

--- models/Book.js
+++ models/Book.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose');
 
-const BookSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+const BookSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    bookId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    authors: [String],
+    status: {
+      type: String,
+      enum: ['reading', 'completed', 'planning'],
+      required: true,
+    },
   },
-  bookId: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  authors: [String],
-  status: {
-    type: String,
-    enum: ['reading', 'completed', 'planning'],
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Book', BookSchema);
